bd6e9e891a759ee9b40b161ef73d6865ae10af34
Halil Dogan <33289@ma-web.nl>
2022-03-25 12:06:47 +0100

    datum

--- js/main.js
+++ js/main.js
@@ -87,3 +87,8 @@ var myChart = new Chart(cntxt, {
 });
 
 
+let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + ' ' + cTime;
+console.log(dateTime);
